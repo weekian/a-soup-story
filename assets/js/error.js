a8ee957de13d9ba0c930fe9a1a2d56b8ae40f404
asoupstory <asoupstory@gmail.com>
2017-06-24 22:44:14 +0800

    404.html JS changes
    - Uncommenting of timeout

--- assets/js/error.js
+++ assets/js/error.js
@@ -1,8 +1,7 @@
 ---
 ---
 $(document).ready(function(){
-  //Commenting out of timeout function to automatically redirect for development purposes
-  //setTimeout(function(){
-  //    window.location.href= "{{"" | absolute_url}}";
-  //}, 3000);
+  setTimeout(function(){
+      window.location.href= "{{"" | absolute_url}}";
+  }, 3000);
 });
