dceb762cf910f03dd5ff353b94537fcd401647ef
asoupstory <asoupstory@gmail.com>
2017-06-24 22:49:12 +0800

    HTTPS Fix
    - Inclusion of secure variable assignment

--- assets/js/error.js
+++ assets/js/error.js
@@ -1,5 +1,10 @@
 ---
 ---
+{% if site.url == "http://localhost:4000" or site.url contains "https" %}
+	{% assign secure = "http" %}
+{% else %}
+	{% assign secure = "https" %}
+{% endif %}
 $(document).ready(function(){
   setTimeout(function(){
       window.location.href= "{{"" | absolute_url | replace: "http", secure }}";
